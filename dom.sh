c7a6b3cb9c69cc9cf55e2ef45eb17f19b03499c9
Hussein Ali Taha <69895843+husseinphp@users.noreply.github.com>
2022-09-18 17:17:26 +0300

    Create dom.sh

---
+++ dom.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#install subfinder
+docker pull projectdiscovery/subfinder
+#alias subfinder
+alias subfinder='docker run -it --rm -w /data -v $(pwd):/data projectdiscovery/subfinder'
+#install httpx
+docker pull projectdiscovery/httpx
+#install nuclei
+docker pull projectdiscovery/nuclei
+#alias httpx
+alias httpx='docker run -it --rm -w /data -v $(pwd):/data projectdiscovery/httpx'
+#alias nuclei
+alias nuclei='docker run -it --rm -w /data -v $(pwd):/data projectdiscovery/nuclei'
+
+echo "subfinder & httpx &nuclei  successfully installs "
+
+
+#enumeration subdomain
+subfinder -d $1 -silent >> $1subdomain.txt
+ 
+
+wc -l <  $1subdomain.txt 
+ echo "enumeration subdomain  successfully  " 
+
+
+echo "enumeration httpx  successfully  " 
+httpx -l $1subdomain.txt  -o  $1live.txt
+
+wc -l <  $1live.txt
+
+#DOM-XSS-CVE-2021-24891
+nuclei -list $1live.txt -t "cves/2021/CVE-2021-24891.yaml"
+
